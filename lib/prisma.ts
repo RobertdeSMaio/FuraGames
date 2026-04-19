511629f188c893483f63e64b78da6d36ecd2efa3
Robert de S. Maio <robertmaio@outlook.com>
2026-04-19 18:44:09 -0300

    erro entrar no edit

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,6 @@
-const { PrismaClient } = require("@prisma/client");
+import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: any };
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ??
